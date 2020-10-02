644d97ba04b4227e7d19ed4a7cc6a175c859b071
Jorge <yBoQyUyz?)Yf>
2020-10-01 20:02:44 -0500

    Secciones de productos

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 $(function(){
     $('.esResponsiv').each(function(i, img){
         var fc = parseInt($(img).css('max-width')) / $(img).height();
-        console.log(fc);
         responsivImg($(img), fc);
         $(window).resize(function() {
             responsivImg($(img), fc);
         });
     })
     $('.dimerSlider').each(function(i, slider){
-        var {dimerAutomatico, dimerDuracion, dimerControls, dimerTimebar} = $(slider).data();
+        var {dimerAutomatico, dimerDuracion, dimerControls} = $(slider).data();
         var slide = $(slider).find('.dimerSlide');
         var totales = slide.length;
         if (totales > 0) slide.first().addClass('activo');
         if (totales > 1) {
             if (dimerControls) {
                 var wrapperDots = $('.dimerSlider').find('.dimerDots');
-                $(slider).append('<div class="manualControls"><div class="dimerControl control at"></div><div class="dimerControl control av"></div></div>');
+                $(slider).append('<div class="manualControls"><div class="dimerControl control circular at"></div><div class="dimerControl control circular av"></div></div>');
                 if (wrapperDots.length) {
                     var dots = wrapperDots;
                 }else {
@@ -45,12 +44,51 @@ $(function(){
                     autoSlider($(slider), totales, dimerControls)
                 }, dimerDuracion || 5000);
             }
-            if (dimerTimebar) {
-                $(slider).append('<div class="dimerBar"></div>');
-            }
         } else {
-            console.log($(slide).css('transition', 'none'));
+            $(slide).css('transition', 'none')
         }
+    })
+    $('.sliderX').each(function(i, sliderX){
+        var wrapper = $(sliderX).find('.wrapperX');
+        var totalSlides = $(sliderX).find('.slideX').length;
+        var widthSlider = wrapper[0].getBoundingClientRect().width;
+        var widthSlide = $(sliderX).find('.slideX').first()[0].getBoundingClientRect().width;
+        var groupShow = 1;
+        var slidesXgrupo = widthSlider / widthSlide;
+        slidesXgrupo = Math.floor(slidesXgrupo);
+        var grupos = totalSlides / slidesXgrupo;
+        grupos = Math.ceil(grupos);
+        if (grupos > 1) {
+            $(sliderX).find('.wrapperSlider').append('<div class="controlesX"><div class="controlX at"></div><div class="controlX av"></div></div>');
+            $(window).resize(function() {
+                groupShow = 1;
+                widthSlider = wrapper[0].getBoundingClientRect().width;
+                widthSlide = $(sliderX).find('.slideX').first()[0].getBoundingClientRect().width;
+                slidesXgrupo = Math.floor(widthSlider / widthSlide);
+                grupos = Math.ceil(totalSlides / slidesXgrupo);
+                wrapper.css('transform', 'translateX(0px)');
+            });
+            $(sliderX).on('click', '.controlesX .controlX', function(){
+                if ($(this).hasClass('av')) {
+                    if (groupShow == grupos) groupShow = 0;
+                    wrapper.css('transform', `translateX(-${widthSlider * groupShow}px)`);
+                    groupShow++;
+                }else if ($(this).hasClass('at')) {
+                    groupShow = groupShow - 1;
+                    if (groupShow == 0) groupShow = grupos;
+                    wrapper.css('transform', `translateX(-${widthSlider * (groupShow - 1)}px)`);
+                }
+            })
+        }
+    })
+    
+    $('.tioDimer_dropdown').click(function(){
+        $(this).children('.dropOptions').slideToggle('fast');
+        $(this).toggleClass('activo');
+    })
+    $('.tioDimer_dropdown .dropOptions .option').click(function(){
+        let value = $(this).data('value');
+        $(this).parents('.tioDimer_dropdown').find('.currentValue').text(value);
     })
 
     function autoSlider(slider, totalSlides, indicadores, avanzar = true){
